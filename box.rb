338f6ff2a857cdb1abf44bf51c025ba6493f365e
nikhilsk <47417007+nikhilsk@users.noreply.github.com>
2025-09-12 19:22:21 +0530

    [REL prep] image urls to be configured for helm prep (#140)

--- box.rb
+++ box.rb
@@ -22,6 +22,10 @@ run "chmod +x /usr/bin/asset-push"
 copy "asset-build/configmanager-asset-push.sh", "/configmanager-asset-push.sh"
 run "chmod +x /configmanager-asset-push.sh"
 
+# install yq
+run "wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64"
+run "chmod a+x /usr/local/bin/yq"
+
 if user == "root"
   # update user .bash_profile
   run "echo 'export GOPATH=/usr' >> /root/.bash_profile"
